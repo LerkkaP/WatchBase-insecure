3db7029207d87885265cf4a72a7f64ba629e305c
Erik Peteri <peteri.erik@gmail.com>
2023-08-15 11:14:39 +0300

    configured routing

--- app/views.py
+++ app/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, "home.html")
+
+def login(request):
+    return render(request, "login.html")
+
+
