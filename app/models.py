00180dd8440232a43097928d8279eaa88a1856fb
Erik Peteri <peteri.erik@gmail.com>
2023-08-15 17:29:30 +0300

    database for watches and new url

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Watch(models.Model):
+    brand = models.CharField(max_length=200)
+    price = models.IntegerField()
+    model = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.brand
+    
+    def watch_price(self):
+        return self.price
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Watch
 
 # Create your views here.
 def home(request):
@@ -7,4 +8,7 @@ def home(request):
 def login(request):
     return render(request, "login.html")
 
+def watches(request):
+    items = Watch.objects.all()
+    return render(request, "watches.html", {"watches": items})
 
