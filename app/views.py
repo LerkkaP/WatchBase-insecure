bc87cbd2999be0c215e136f0ab79d00a6fd202d3
Erik Peteri <peteri.erik@gmail.com>
2023-08-16 13:54:30 +0300

    insecure login functionality with session implemented

--- app/views.py
+++ app/views.py
@@ -2,29 +2,40 @@ from django.shortcuts import render
 from .models import Watch
 from django.views.decorators.csrf import csrf_exempt
 from django.db import connection
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    context = {'is_authenticated': request.session.get('username')}
+    return render(request, "home.html", context)
 
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
         user = request.POST.get('username')
         password = request.POST.get('password')
+        
         with connection.cursor() as cursor:
-            cursor.execute('SELECT * FROM app_user')
-            for row in cursor:
-                user_real = row[1]
-                password_real = row[2]
-        if user == user_real and password == password_real:
-            print("toimii")
+            cursor.execute(f"SELECT * FROM app_user WHERE username = '{user}'")
+            row = cursor.fetchone()
+        
+        if row and row[2] == password and row[1] == user:
+            request.session['username'] = user
+            return redirect('home')
         else:
-            print("incorrect password or username")
-
+            messages.success(request, ('Invalid username or password'))
+            return redirect('login')
     return render(request, "login.html")
+
+@csrf_exempt
+def logout(request):
+    if 'username' in request.session:
+        del request.session['username']
+    return redirect('login')
 
 def watches(request):
     items = Watch.objects.all()
     return render(request, "watches.html", {"watches": items})
+
 
