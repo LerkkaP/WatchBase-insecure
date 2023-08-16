5728cde4730e71c3d05ecdd048c25926cb05fb8a
Erik Peteri <peteri.erik@gmail.com>
2023-08-16 11:07:29 +0300

    disabled csrf_token for demonstrating purposes and made login logic

--- app/views.py
+++ app/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import Watch
+from django.views.decorators.csrf import csrf_exempt
+from django.db import connection
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
+@csrf_exempt
 def login(request):
+    if request.method == 'POST':
+        user = request.POST.get('username')
+        password = request.POST.get('password')
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM app_user')
+            for row in cursor:
+                user_real = row[1]
+                password_real = row[2]
+        if user == user_real and password == password_real:
+            print("toimii")
+        else:
+            print("incorrect password or username")
+
     return render(request, "login.html")
 
 def watches(request):
