dadb854f8e3fde42d65732c83e2db9e871ae1b7c
Erik Peteri <peteri.erik@gmail.com>
2023-08-17 15:44:36 +0300

    added description row to watches

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class Watch(models.Model):
     brand = models.CharField(max_length=200)
     price = models.IntegerField()
     model = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
 
     def __str__(self):
         return self.brand
